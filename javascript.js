6a064e2a848d1fe19777293a3604581f51a7a3ce
Programivan <91947344+Programivan@users.noreply.github.com>
2023-04-03 21:14:24 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 var mine = require("Mineflayer")
 
 var bot = mine.createBot({host: "0.tcp.eu.ngrok.io",
-#     port:"16936",
-#     version:"1.12.2",
-#     username:"bot"})
+     port:"16936",
+     version:"1.12.2",
+     username:"bot"})
+
+alert("hello")
